de5ae31e2f30a35b38be611eae90d270bc6ad1b9
lrounin <lrounin@gmail.com>
2016-01-26 01:05:22 +0000

    Add basic comment functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :owned_post, only: [:edit, :update, :destroy]
+  
   def index
     @posts = Post.all
   end
@@ -52,6 +53,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
 end
