ad68acbd9ced9e51608f5f682094d0dba2998f10
lrounin <lrounin@gmail.com>
2016-02-29 22:14:36 +0000

    Fix Edit Post image duplication, add pagination to posts and comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
+
   def alert_for(flash_type)
     { success: 'alert-success',
       error: 'alert-danger',
       alert: 'alert-warning',
       notice: 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def form_image_select(post)
+    return image_tag post.image.url(:medium), id: 'image-preview', class: 'img-responsive form--new-post__image' if post.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive form--new-post__image'
   end
 end
