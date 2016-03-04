a7fea6491b5943b7483e69df0374d27f42e09423
lrounin <lrounin@gmail.com>
2016-03-04 22:38:23 +0000

    Add profile functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,10 @@ module ApplicationHelper
     return image_tag post.image.url(:medium), id: 'image-preview', class: 'img-responsive form--new-post__image' if post.image.exists?
     image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive form--new-post__image'
   end
+
+  def profile_avatar_select(user)
+    return image_tag user.avatar.url(:medium), id: 'image-preview', class: 'img-responsive img-circle' if user.avatar.exists?
+    image_tag 'default-avatar.jpg', id: 'image-preview', class: 'img-responsive img-circle'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   
   resources :posts do
@@ -7,5 +9,11 @@ Rails.application.routes.draw do
   end
   
   root 'posts#index'
+
+  get ':user_name', to: 'profiles#show', as: :profile
+
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
 
 end
